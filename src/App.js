0212b77d6862dc88bde5c627064bbf91f0cf73e7
dezordithomas <dezordi.thomas@gmail.com>
2019-02-04 12:40:07 +0100

    add pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
-import { NavigationDrawer } from 'react-md';
-import logo from './logo.svg';
-import './App.scss';
+import { Route, Switch } from 'react-router-dom';
+import NavigationDrawer from 'react-md/lib/NavigationDrawers';
+import NavLink from './NavLink';
+
+import Home from './pages/Home';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+import Page3 from './pages/Page3';
+
+const navItems = [{
+  exact: true,
+  label: 'Home',
+  to: '/',
+  icon: 'home',
+}, {
+  label: 'Page 1',
+  to: '/page-1',
+  icon: 'bookmark',
+}, {
+  label: 'Page 2',
+  to: '/page-2',
+  icon: 'donut_large',
+}, {
+  label: 'Page 3',
+  to: '/page-3',
+  icon: 'flight_land',
+}];
 
 class App extends Component {
   render() {
     return (
-      <NavigationDrawer
-        toolbarTitle="react-md with create-react-app v2"
-        drawerTitle="react-app"
-      >
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </NavigationDrawer>
+      <Route
+        render={({ location }) => (
+          <NavigationDrawer
+            drawerTitle="react-md with CRA"
+            toolbarTitle="Welcome to react-md"
+            navItems={navItems.map(props => <NavLink {...props} key={props.to} />)}
+          >
+            <Switch key={location.key}>
+              <Route exact path="/" location={location} component={Home} />
+              <Route path="/page-1" location={location} component={Page1} />
+              <Route path="/page-2" location={location} component={Page2} />
+              <Route path="/page-3" location={location} component={Page3} />
+            </Switch>
+          </NavigationDrawer>
+        )}
+      />
     );
   }
 }
